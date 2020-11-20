c032d892e5021666a95471f2620c04688670fd89
keemtj <keemgreat@gmail.com>
2020-11-20 22:25:27 +0900

    :sparkles: CSS Variables and JS

--- 03 - CSS Variables/index.js
+++ 03 - CSS Variables/index.js
@@ -1,1 +1,12 @@
 // code
+const inputs = document.querySelectorAll(".controls input");
+
+const handleUpdate = ({ target }) => {
+  const { dataset, name, value } = target;
+  const suffix = dataset.sizing || "";
+
+  document.documentElement.style.setProperty(`--${name}`, value + suffix);
+};
+
+inputs.forEach((input) => input.addEventListener("change", handleUpdate));
+inputs.forEach((input) => input.addEventListener("mousemove", handleUpdate));
