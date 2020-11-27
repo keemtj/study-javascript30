652ae5740250101759d75f685de8c4c63bee0ee0
keemtj <keemgreat@gmail.com>
2020-11-27 14:09:45 +0900

    :sparkles: Fun with HTML5 Canvas

--- 08 - Fun with HTML5 Canvas/index.js
+++ 08 - Fun with HTML5 Canvas/index.js
@@ -1,1 +1,55 @@
 // code
+const canvas = document.querySelector("#draw");
+const ctx = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+// https://mothereffinghsl.com
+ctx.strokeStyle = "#BADA55";
+ctx.lineJoin = "round";
+ctx.lineCap = "round";
+ctx.lineWidth = 100;
+// ctx.globalCompositeOperation = "multiply";
+
+let isDrawing = false;
+// need a starting X and Y and ending X and Y
+// let lastX = 0;
+// let lastY = 0;
+let [lastX, lastY] = [0, 0];
+let hue = 0;
+let direction = true;
+
+function draw(e) {
+  if (!isDrawing) return; // stop the from running when they are not moused down
+  console.log(e);
+  ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+
+  ctx.beginPath();
+  ctx.moveTo(lastX, lastY);
+  ctx.lineTo(e.offsetX, e.offsetY);
+  ctx.stroke();
+  // lastX = e.offsetX;
+  // lastY = e.offsetY;
+  [lastX, lastY] = [e.offsetX, e.offsetY];
+
+  hue++;
+  hue %= 360;
+  if (ctx.lineWidth >= 100 || ctx.lineWidth <= 1) {
+    direction = !direction;
+  }
+  if (direction) {
+    ctx.lineWidth++;
+  } else {
+    ctx.lineWidth--;
+  }
+}
+
+canvas.addEventListener("mousemove", draw);
+canvas.addEventListener("mousedown", (e) => {
+  isDrawing = true;
+  // 마우스다운 이벤트가 일어난 시점에 e.offsetX, e.offsetY로 업데이트
+  [lastX, lastY] = [e.offsetX, e.offsetY];
+});
+canvas.addEventListener("mouseup", () => (isDrawing = false));
+canvas.addEventListener("mouseout", () => (isDrawing = false));
