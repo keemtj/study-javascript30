5518c49913b024b7d1d465ede346ae82cb4ee30e
keemtj <keemgreat@gmail.com>
2020-12-07 10:26:55 +0900

    :tada: Initialize Unreal Webcam Fun

---
+++ 19 - Unreal Webcam Fun/index.js
@@ -0,0 +1,6 @@
+// code
+const video = document.querySelector(".player");
+const canvas = document.querySelector(".photo");
+const ctx = canvas.getContext("2d");
+const strip = document.querySelector(".strip");
+const snap = document.querySelector(".snap");
