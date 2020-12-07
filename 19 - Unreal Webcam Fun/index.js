5d1c25c8a9b3900421fb51d8d4d1256a4f263a03
keemtj <keemgreat@gmail.com>
2020-12-07 12:28:32 +0900

    :construction: Use async-await instead of promise

--- 19 - Unreal Webcam Fun/index.js
+++ 19 - Unreal Webcam Fun/index.js
@@ -7,23 +7,39 @@ const ctx = canvas.getContext("2d");
 const strip = document.querySelector(".strip");
 const snap = document.querySelector(".snap");
 
-const getVideo = () => {
-  navigator.mediaDevices
-    .getUserMedia({ video: true, audio: false })
-    .then((localMediaStream) => {
-      console.log(localMediaStream);
-      //  DEPRECIATION :
-      //  The following has been depreceated by major browsers as of Chrome and Firefox.
-      //  video.src = window.URL.createObjectURL(localMediaStream);
-      //  Please refer to these:
-      //  Deprecated  - https://developer.mozilla.org/en-US/docs/Web/API/URL/createObjectURL
-      //  Newer Syntax - https://developer.mozilla.org/en-US/docs/Web/API/HTMLMediaElement/srcObject
-      video.srcObject = localMediaStream;
-      video.play();
-    })
-    .catch((e) => {
-      console.error(`OH NO!!!`, e);
+// promise
+// const getVideo = () => {
+//   navigator.mediaDevices
+//     .getUserMedia({ video: true, audio: false })
+//     .then((localMediaStream) => {
+//       console.log(localMediaStream);
+//       //  DEPRECIATION :
+//       //  The following has been depreceated by major browsers as of Chrome and Firefox.
+//       //  video.src = window.URL.createObjectURL(localMediaStream);
+//       //  Please refer to these:
+//       //  Deprecated  - https://developer.mozilla.org/en-US/docs/Web/API/URL/createObjectURL
+//       //  Newer Syntax - https://developer.mozilla.org/en-US/docs/Web/API/HTMLMediaElement/srcObject
+//       video.srcObject = localMediaStream;
+//       video.play();
+//     })
+//     .catch((e) => {
+//       console.error(`OH NO!!!`, e);
+//     });
+// };
+
+// async-await
+const getVideo = async () => {
+  let localMediaStream = null;
+  try {
+    localMediaStream = await navigator.mediaDevices.getUserMedia({
+      video: true,
+      audio: false,
     });
+    video.srcObject = localMediaStream;
+    video.play();
+  } catch (e) {
+    console.log(e);
+  }
 };
 
 const redEffect = (pixels) => {
