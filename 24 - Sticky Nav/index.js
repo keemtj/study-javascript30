79746882cec330f754a6a756ff8b2f6f34ca4401
keemtj <keemgreat@gmail.com>
2020-12-09 08:33:39 +0900

    :sparkles: Sticky Nav

--- 24 - Sticky Nav/index.js
+++ 24 - Sticky Nav/index.js
@@ -1,1 +1,16 @@
 // code
+const nav = document.querySelector("#main");
+const topOfNav = nav.offsetTop;
+
+const fixNav = () => {
+  // console.log(topOfNav, window.scrollY);
+  if (window.scrollY >= topOfNav) {
+    document.body.style.paddingTop = `${nav.offsetHeight}px`;
+    document.body.classList.add("fixed-nav");
+  } else {
+    document.body.style.paddingTop = 0;
+    document.body.classList.remove("fixed-nav");
+  }
+};
+
+window.addEventListener("scroll", fixNav);
