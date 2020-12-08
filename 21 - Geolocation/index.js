7361ae1cf0cd493960b89ccd52fe39220fac2de2
keemtj <keemgreat@gmail.com>
2020-12-08 14:11:14 +0900

    :sparkles: Geolocation

--- 21 - Geolocation/index.js
+++ 21 - Geolocation/index.js
@@ -1,1 +1,15 @@
 // code
+const arrow = document.querySelector(".arrow");
+const speed = document.querySelector(".speed-value");
+
+navigator.geolocation.watchPosition(
+  (data) => {
+    console.log(data);
+    speed.textContent = Math.round(data.coords.speed);
+    arrow.style.transform = `rotate(${data.coords.heading})`;
+  },
+  (e) => {
+    console.error(e);
+    alert("HEY! YOU GOTTA ALLOW THAT TO HAPPEN!!");
+  }
+);
