a6f73a3ab8925a67a22127d2fdf57ae5bccffc66
keemtj <keemgreat@gmail.com>
2020-12-09 09:11:28 +0900

    :sparkles: Event Capture, Propagation, Bubbling and Once

--- 25 - Event Capture, Propagation, Bubbling and Once/index.js
+++ 25 - Event Capture, Propagation, Bubbling and Once/index.js
@@ -1,1 +1,30 @@
 // code
+const divs = document.querySelectorAll("div");
+const button = document.querySelector("button");
+// const logText = (e) => {
+//   console.log(e.target.classList.value);
+//   e.stopPropagation(); // stop bubbling
+// };
+
+function logText(e) {
+  // console.log(this);
+  console.log(this.classList.value);
+  // e.stopPropagation(); // stop bubbling
+}
+
+// capture: false(default)
+// capture: top to bottom
+// bubbling: bottom to top
+divs.forEach((div) =>
+  div.addEventListener("click", logText, { capture: false, once: true })
+);
+
+// once: div.removeEventListener("click", logText);
+
+button.addEventListener(
+  "click",
+  () => {
+    console.log("click!!!");
+  },
+  { once: true }
+);
